716e0587630c5ec4fa86c9b22154b9c0a10c477b
Zeongwan <503286654@qq.com>
2017-12-12 07:50:45 +0800

    hw9 建立文件

--- MyApplication5/app/src/main/java/com/example/myapplication5/MyViewholder.java
+++ MyApplication5/app/src/main/java/com/example/myapplication5/MyViewholder.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import static com.example.myapplication5.R.styleable.RecyclerView;
+
 /**
  * Created by 丁丁 on 2017/10/21 0021.
  */
---
+++ MyApplication9/app/src/main/java/com/example/myapplication9/User.java
@@ -0,0 +1,8 @@
+package com.example.myapplication9;
+
+/**
+ * Created by 丁丁 on 2017/12/12 0012.
+ */
+
+public class User {
+}
